1d30b80b229ebe5d6b4ef868b0b7e49d76df8044
Sukesh Dash <Sukesh Dash>
2017-06-26 16:40:09 +0100

    Initial Project Scaffolding

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3200;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
